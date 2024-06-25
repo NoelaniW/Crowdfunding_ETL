4c8c8e03fe3e02d908e70e552b9e236fb3f7d2ea
NoelaniW <noe.blackwood6@gmail.com>
2024-06-25 00:19:55 -0700

    updated

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,36 @@
+create table Category (
+    category_ids varchar(100) PRIMARY KEY,
+    category varchar(100)
+);
+
+create table Subcategory (
+    subcategory_ids varchar(100) PRIMARY KEY,
+    subcategory varchar(100)
+);
+
+create table Campaign (
+    cf_id INTEGER,
+    contact_id INTEGER PRIMARY KEY,
+    company_name varchar(100),
+    description_ varchar(100),
+    goal decimal(8,2),
+    pledged decimal(8,2),
+    outcome varchar(100),
+    backers_count int,
+    country varchar(100),
+    currency varchar(100),
+    launched_date date,
+    end_date date,
+    category_ids varchar(100),
+    subcategory_ids varchar(100),
+    foreign key (category_ids) references Category(category_ids),
+    foreign key (subcategory_ids) references Subcategory(subcategory_ids)
+);
+
+create table Contacts (
+    contact_id INTEGER,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(100),
+    foreign key (contact_id) references Campaign(contact_id)
+)
